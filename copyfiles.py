a3add680c7f7a4f5a8fbc6ca967a90f1715fbf88
Andreyvovk18 <69602252+Andreyvovk18@users.noreply.github.com>
2020-08-16 20:21:23 +0300

    First version

--- copyfiles.py
+++ copyfiles.py
@@ -1,6 +1,13 @@
+import os
 import shutil
 
-src = "C:\Users\HOME\Desktop\Test1"
-dst = "C:\Users\HOME\Desktop\Test2"
-shutil.copytree(src=src, dst=dst)
 
+SRC = "D:\\test\\Test1"
+DST = "D:\\test\\Test2"
+
+FILE_NAMES = os.listdir(SRC)
+
+for filename in FILE_NAMES:
+    source_file_path = SRC + "\\" + filename
+    path_to_destination_file = DST + "\\" + filename
+    shutil.copyfile(source_file_path, path_to_destination_file)
